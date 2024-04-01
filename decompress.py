39b93c15da05c9cc1eda65b859a3ba0a378bfcb5
Lukas Zimmermann <lukas@MacBook-Air-von-Lukas.local>
2024-04-02 01:39:02 +0200

    clean uo

--- decompress.py
+++
@@ -1,9 +0,0 @@
-import zlib
-
-with open("data/22557847_detail.zio", "rb") as file:
-    compressed_data = file.read()
-
-decompressed_data = zlib.decompress(compressed_data)
-json_string = decompressed_data.decode()
-
-print(json_string)
